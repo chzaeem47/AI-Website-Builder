bfd2d9e404b34a72905ac845184648fe4940ddb6
chzaeem47 <zaeemshahidak47@gmail.com>
2026-07-11 20:20:31 +0500

    Fixing paths in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv";
 dotenv.config()
-import { connectDB } from "./db/db.js";
+import { connectDB } from "./src/db/db.js";
 import authRouter from './src/routers/auth.route.js'
 import cookieParser from "cookie-parser";
 import cors from 'cors'
