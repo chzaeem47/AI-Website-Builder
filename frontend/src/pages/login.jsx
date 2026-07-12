7229353f0df1ebc2cc6bb7342e8899da616773bf
chzaeem47 <zaeemshahidak47@gmail.com>
2026-07-13 01:11:45 +0500

    fixing bg pics

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -18,7 +18,6 @@ function Login() {
     image-rendering-[crisp-edges] [-webkit-optimize-contrast] image-rendering-[pixelated]'
     style={{backgroundImage: `url(${dark ? "/dark-bg.jpeg" : "/light-bg.jpeg"})`}}>
 
-    <p className='bg-black text-[22px] text-gray-400 font-sans w-150 absolute right-32 bottom-37 text-center'>Turn your ideas into professional websites in minutes Simply type what you want and let AI handle the rest</p>
 
     <button type="button" onClick={() => setDark(!dark)} className="absolute top-5 left-238 w-14 h-14 rounded-full bg-white text-black flex items-center justify-center text-2xl shadow-lg z-50 hover:scale-105 transition">
     {dark ? <FaMoon /> : <FaSun />}
