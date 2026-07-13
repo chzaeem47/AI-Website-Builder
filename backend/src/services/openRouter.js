93d601aecc019ba6332e7387d959bf4990b1b31c
chzaeem47 <zaeemshahidak47@gmail.com>
2026-07-13 23:16:19 +0500

    updating llm

--- backend/src/services/openRouter.js
+++ backend/src/services/openRouter.js
@@ -2,7 +2,7 @@
 
 const openRouterURL = "https://openrouter.ai/api/v1/chat/completions";
 
-const model =  "deepseek/deepseek-chat";
+const model =  "deepseek/deepseek-chat-v3.1";
 
 const generateResponse = async (prompt) => {
 
